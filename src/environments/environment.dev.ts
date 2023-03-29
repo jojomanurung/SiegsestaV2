29e15f4d165d7ca8242e7889b7a8a460dab391f8
Joshua Melvin <joshuamlvn@gmail.com>
2023-03-29 15:04:31 +0700

    Install angular fire

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'siegsesta',
+    appId: '1:408387139975:web:1252d7643a6c620efb7b22',
+    storageBucket: 'siegsesta.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCgY0IpSwOO8pVRxFlmvloZ4gVusLnP5fk',
+    authDomain: 'siegsesta.firebaseapp.com',
+    messagingSenderId: '408387139975',
+    measurementId: 'G-G61LK04FK3',
+  },
+};
